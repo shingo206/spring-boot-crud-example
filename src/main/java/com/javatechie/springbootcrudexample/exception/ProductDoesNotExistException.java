6c0341a8a7cf580df611ac9d240e34ec1a81ab43
Shingo Tamura <shingo206@mac.com>
2020-10-13 21:57:29 +0200

    Implement ProductDoesNotExistException.

--- src/main/java/com/javatechie/springbootcrudexample/exception/ProductDoesNotExistException.java
+++ src/main/java/com/javatechie/springbootcrudexample/exception/ProductDoesNotExistException.java
@@ -8,16 +8,4 @@ public class ProductDoesNotExistException extends RuntimeException {
     public ProductDoesNotExistException() {
         super("The product does not exist.");
     }
-
-    public ProductDoesNotExistException(String message, Throwable throwable) {
-        super(message, throwable);
-    }
-
-    public ProductDoesNotExistException(String message) {
-        super(message);
-    }
-
-    public ProductDoesNotExistException(Throwable throwable) {
-        super(throwable);
-    }
 }
