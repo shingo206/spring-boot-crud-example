12435b80de1b9365b679e7a3ef16664d658d19bf
Shingo Tamura <shingo206@mac.com>
2020-10-16 12:00:01 +0200

    GlobalExceptionHandler changed with prepareResponse.

--- src/main/java/com/javatechie/springbootcrudexample/exception/ResourceNotFoundException.java
+++ src/main/java/com/javatechie/springbootcrudexample/exception/ResourceNotFoundException.java
@@ -3,10 +3,8 @@ package com.javatechie.springbootcrudexample.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.io.Serializable;
-
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException implements Serializable {
+public class ResourceNotFoundException extends RuntimeException {
     private static final long serialVersionUID = -5554702057444971321L;
 
     public ResourceNotFoundException(String message) {
